14df77ff49da04e1f926eca2d426612ea13663f4
Jeff_Lee_0228 <suam3282@gmail.com>
2020-10-29 10:11:09 -0500

    feat: added first round page

--- client/Main.js
+++ client/Main.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import FirstRound from './components/FirstRound';
 import LandingPage from './components/LandingPage';
 import SecondRound from './components/SecondRound';
-import ThirdRound from './components/ThirdRound';
 import Score from './components/Score';
 import NoneFound from './components/NoneFound';
 
@@ -15,7 +14,6 @@ export default class Main extends Component {
           <Route exact path="/" component={LandingPage} />
           <Route path="/first" component={FirstRound} />
           <Route path="/second" component={SecondRound} />
-          <Route path="/third" component={ThirdRound} />
           <Route path="/score" component={Score} />
           <Route component={NoneFound} />
         </Switch>
