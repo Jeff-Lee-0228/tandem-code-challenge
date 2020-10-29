0587ee625d7eb1b05643347ba117724176748365
Jeff_Lee_0228 <suam3282@gmail.com>
2020-10-28 19:07:13 -0500

    feat: adding components

--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,20 @@
+import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
+import FirstRound from './components/FirstRound';
+import LandingPage from './components/LandingPage';
 
 export default class Main extends Component {
   render() {
     return (
       <div id="main">
         <h1>Welcome to Code Challenge</h1>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/first" component={FirstRound} />
+          {/* <Route path="/2" component={SecondRound} />
+        <Route path="/3" component={ThirdRound} />
+      <Route path="/score" component={Score} /> */}
+        </Switch>
       </div>
     );
   }
---
+++ client/components/FirstRound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FirstRound extends Component {
+  render() {
+    return (
+      <div>
+        <h2> First Round!</h2>
+      </div>
+    );
+  }
+}
