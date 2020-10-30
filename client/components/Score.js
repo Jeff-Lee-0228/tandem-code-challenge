f87e28db63b434ca57ba1b53f67d5974cea4bd31
Jeff_Lee_0228 <suam3282@gmail.com>
2020-10-30 09:15:39 -0500

    feat: score page styling

--- client/components/Score.js
+++ client/components/Score.js
@@ -19,11 +19,44 @@ class Score extends Component {
       <Container className="container">
         <Card bg="light">
           <Card.Header>Trivia Completed! </Card.Header>
+          <Card.Img
+            className="score-image"
+            variant="top"
+            src="https://upload-icon.s3.us-east-2.amazonaws.com/uploads/icons/png/7795702121539683258-512.png"
+          />
           <Card.Body key="score">
-            <Card.Title> Your score is : {finalScore} / 20 </Card.Title>
-            <Card.Text>
-              <strong>hi</strong>
-            </Card.Text>
+            <Card.Title>
+              {' '}
+              <b>Your score is : {finalScore} / 20</b>{' '}
+            </Card.Title>
+            {(() => {
+              switch (finalScore) {
+                case 10 < finalScore <= 15:
+                  return (
+                    <Card.Text>
+                      <div>
+                        <b>Almost there.</b>
+                      </div>
+                    </Card.Text>
+                  );
+                case 15 < finalScore:
+                  return (
+                    <Card.Text>
+                      <div>
+                        <b>Congratulations. You are very smart!</b>
+                      </div>
+                    </Card.Text>
+                  );
+                default:
+                  return (
+                    <Card.Text>
+                      <div>
+                        <b>Why don't you play again?</b>
+                      </div>
+                    </Card.Text>
+                  );
+              }
+            })()}
           </Card.Body>
           <Button variant="primary" type="button" className="button-home">
             <Link className="begin-button" to="/">
